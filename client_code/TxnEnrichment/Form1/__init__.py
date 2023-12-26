1d8ea0ef7a4bd96b8fa27215714e56b2c4d412ea
internal.tools@opplane.com <internal.tools@opplane.com>
2023-12-26 22:44:28 +0000

    Created TxnEnrichment.Form1, TxnEnrichment/.gitkeep, TxnEnrichment/Arrow 16.svg, TxnEnrichment/Vector.svg and 1 more; Edited TxnEnrichment, theme.css and settings

--- client_code/TxnEnrichment/Form1/__init__.py
+++ client_code/TxnEnrichment/Form1/__init__.py
@@ -1,10 +1,12 @@
-from ._anvil_designer import TxnEnrichmentTemplate
+from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
 
-class TxnEnrichment(TxnEnrichmentTemplate):
+class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.drop_down_1.role = 'dropdown-style'
+
 
     # Any code you write here will run before the form opens.
