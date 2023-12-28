d63e7283ce75f1a26bb15b0fc2bc85c09f9d61b9
internal.tools@opplane.com <internal.tools@opplane.com>
2023-12-28 17:27:36 +0000

    Edited TxnEnrichment

--- client_code/TxnEnrichment/__init__.py
+++ client_code/TxnEnrichment/__init__.py
@@ -11,8 +11,11 @@ class TxnEnrichment(TxnEnrichmentTemplate):
     # Any code you write here will run before the form opens.
   
   def enrich_click(self, **event_args):
-  """This method is called when the enrich is clicked"""
-  form = get_open_form()
-  form.response_panel.clear()
-  response_component = Response()
-  form.response_panel.add_component(response_component)
+    form = get_open_form()
+    form.content_panel.response_panel.clear()
+    response_component = Response()
+    form.content_panel.response_panel.add_component(response_component)
+
+  def outlined_button_1_copy_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
