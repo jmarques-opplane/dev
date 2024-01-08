ff70de1f2dede430aa5caf128c54011f427d1191
internal.tools@opplane.com <internal.tools@opplane.com>
2024-01-08 12:17:41 +0000

    Edited Nudges and settings

--- client_code/Nudges/__init__.py
+++ client_code/Nudges/__init__.py
@@ -2,6 +2,7 @@ from ._anvil_designer import NudgesTemplate
 from anvil import *
 import anvil.server
 from ..NudgesInput import NudgesInput
+from ..PaymentsInput import PaymentsInput
 
 class Nudges(NudgesTemplate):
   def __init__(self, **properties):
@@ -12,6 +13,12 @@ class Nudges(NudgesTemplate):
     form = get_open_form()
     form.content_panel.clear()
     form.content_panel.add_component(NudgesInput())
+
+  def payments_button_click(self, **event_args):
+    form = get_open_form()
+    form.content_panel.clear()
+    form.content_panel.add_component(PaymentsInput())
+    pass
 
  
 
