e3c72ae4bfab07b071296f199f62b20e5508fcb9
internal.tools@opplane.com <internal.tools@opplane.com>
2024-01-16 15:27:25 +0000

    Edited NewForm and Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -8,9 +8,11 @@ class Form2(Form2Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
+    # Add JavaScript event listener to the button
+    self.dom_nodes["myButton"].addEventListener('click', self.add_new_form)
     # Any code you write here will run before the form opens.
 
-  def add_new_form(self):
+  def add_new_form(self, event):
     new_form_instance = NewForm()
     self.content_panel.clear()
     self.content_panel.add_component(new_form_instance)
