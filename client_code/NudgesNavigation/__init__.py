99c735c8f325edb0689a8896f09e311787a9b1fd
internal.tools@opplane.com <internal.tools@opplane.com>
2024-01-18 14:06:25 +0000

    Edited NudgesNavigation

--- client_code/NudgesNavigation/__init__.py
+++ client_code/NudgesNavigation/__init__.py
@@ -6,5 +6,10 @@ class NudgesNavigation(NudgesNavigationTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.flow_panel_1.w
 
     # Any code you write here will run before the form opens.
+
+  def subscriptions_payments_copy_3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
