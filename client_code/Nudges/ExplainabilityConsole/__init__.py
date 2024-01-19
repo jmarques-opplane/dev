c63e73a2be4d2346bd2893fff656462869518616
internal.tools@opplane.com <internal.tools@opplane.com>
2024-01-19 15:00:50 +0000

    Created Nudges.ExplainabilityConsole2; Edited Nudges.ExplainabilityConsole; Deleted Nudges.ExplainabilityConsole_copy

--- client_code/Nudges/ExplainabilityConsole/__init__.py
+++ client_code/Nudges/ExplainabilityConsole/__init__.py
@@ -1,6 +1,7 @@
 from ._anvil_designer import ExplainabilityConsoleTemplate
 from anvil import *
 import anvil.server
+from ..ExplainabilityConsole2 import ExplainabilityConsole2
 
 class ExplainabilityConsole(ExplainabilityConsoleTemplate):
   def __init__(self, **properties):
@@ -8,3 +9,7 @@ class ExplainabilityConsole(ExplainabilityConsoleTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+  
+  def data_lineage_click(self, **event_args):
+    self.parent.linear_panel.content_panel.clear()
+    self.parent.linear_panel.content_panel.add_component(ExplainabilityConsole2())
--- client_code/Nudges/ExplainabilityConsole2/__init__.py
+++ client_code/Nudges/ExplainabilityConsole2/__init__.py
@@ -1,8 +1,8 @@
-from ._anvil_designer import ExplainabilityConsole_copyTemplate
+from ._anvil_designer import ExplainabilityConsole2Template
 from anvil import *
 import anvil.server
 
-class ExplainabilityConsole_copy(ExplainabilityConsole_copyTemplate):
+class ExplainabilityConsole2(ExplainabilityConsole2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
