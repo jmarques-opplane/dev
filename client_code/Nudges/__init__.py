b4dda1dff2ef76221c0ab4fe778c1fb6eb65635f
internal.tools@opplane.com <internal.tools@opplane.com>
2024-01-19 12:06:33 +0000

    Created Nudges.SalaryDeposits; Edited Nudges

--- client_code/Nudges/__init__.py
+++ client_code/Nudges/__init__.py
@@ -5,18 +5,20 @@ from ..NudgesInput import NudgesInput
 from ..PaymentsInput import PaymentsInput
 from ..ExplainabilityConsole_1 import ExplainabilityConsole_1
 from .SubscriptionPayments import SubscriptionPayments
+from .SalaryDeposits import SalaryDeposits
 
 class Nudges(NudgesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
   def see_how_it_works_click(self, **event_args):
     self.content_panel.clear()
     self.content_panel.add_component(SubscriptionPayments())
 
-  def payments_button_click(self, **event_args):
+  def salary_deposits_click(self, **event_args):
     self.content_panel.clear()
-    #form.content_panel.add_component(PaymentsInput())
+    self.content_panel.add_component(SalaryDeposits())
 
   def xai_button_click(self, **event_args):
     form = get_open_form()
