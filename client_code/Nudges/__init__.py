adf9fd6eb02a881e70528084a9575e019326cedc
internal.tools@opplane.com <internal.tools@opplane.com>
2024-01-19 18:19:01 +0000

    Edited Nudges

--- client_code/Nudges/__init__.py
+++ client_code/Nudges/__init__.py
@@ -12,12 +12,21 @@ class Nudges(NudgesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+
+  def reset_links(self, **event_args):
+    self.salary_deposits.role = 'elevated-button'
+    self.subscriptions_payments.role = 'elevated-button'
     
   def see_how_it_works_click(self, **event_args):
+    self.reset_links()
+    self.subscriptions_payments.role = 'selected'
     self.content_panel.clear()
     self.content_panel.add_component(SubscriptionPayments())
 
   def salary_deposits_click(self, **event_args):
+    self.reset_links()
+    self.salary_deposits.role = 'filled-button'
+    self.salary_deposits.background = "#217bf4"
     self.content_panel.clear()
     self.content_panel.add_component(SalaryDeposits())
 
@@ -26,6 +35,8 @@ class Nudges(NudgesTemplate):
     self.content_panel.add_component(ExplainabilityConsole())
 
   def subscriptions_payments_click(self, **event_args):
+    self.reset_links()
+    self.subscriptions_payments.role = 'selected'
     self.content_panel.clear()
     self.content_panel.add_component(SubscriptionPayments())
 
