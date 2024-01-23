7182410ab4a843172eab83e04c7582092c461d27
internal.tools@opplane.com <internal.tools@opplane.com>
2024-01-23 14:17:18 +0000

    Edited ServerModule

--- server_code/ServerModule.py
+++ server_code/ServerModule.py
@@ -65,7 +65,7 @@ def call_insights_api(account_id, time_period, subscription_type):
     }
 
     def format_subscription_details_as_html(response_json):
-        html_output = "<ul>"
+        html_output = '<ul style="color: "#646464">'
 
         if 'results' not in response_json or not response_json['results']:
             return "Invalid Account ID."
